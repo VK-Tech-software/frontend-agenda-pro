d16292d01587bfa04f19d931d856724beb1e63b4
VictorKlitzke <victorklitzke2@gmail.com>
2026-02-22 18:49:43 -0400

    feat: Mostrando o plano, mostrando dados da empresa

---
+++ src/feature/auth/pages/components/register/register-form-page.tsx
@@ -0,0 +1,10 @@
+export const RegisterFormPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            <div className="w-full rounded-lg bg-white p-6 shadow-md">
+                <h2 className="mb-6 text-center text-2xl font-bold">Registrar</h2>
+              
+            </div>
+        </div>
+    );
+}
